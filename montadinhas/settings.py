35d63e023fe547d80e2ac558c02987097adf9249
Vinny084 <josevinicius195@gmail.com>
2023-12-14 11:47:34 -0300

    static

--- montadinhas/settings.py
+++ montadinhas/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_ROOT= os.path.join(BASE_DIR,'static')
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'montadinhas/static')]
